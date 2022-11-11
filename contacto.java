ac7a4b7010ad0c64c605ccc2c46e6a532c93b4f7
Erick Barrera <ebarrera-2019263@kinal.edu.gt>
2022-11-11 12:27:50 -0600

    se implemeto atributos a contacto

--- contacto.java
+++ contacto.java
@@ -1,3 +1,37 @@
 public class contacto {
+    private String nombre;
+    private String numero;
+
+
+    public contacto() {
+        this.nombre = "";
+        this.numero = "";     
+    }
+    public contacto(String string, String string2) {
+        this.nombre = string;
+        this.numero = string2;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String string) {
+        this.nombre = string;
+    }
+
+    public String getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(String string) {
+        this.numero = string;
+    }
+
+    public String toString() {
+        return "Nombre:" + this.getNombre() + "\nN\u00famero: " + this.getNumero() + "\n";
+    }
+
+
     
 }
---
+++ modo_reproduccion.java
@@ -0,0 +1,3 @@
+public class modo_reproduccion {
+    
+}
